79626884a52441f9f06f990a37b2b3b5c336a16a
Lim Jet <57783762+daoauth@users.noreply.github.com>
2025-05-04 06:58:33 +0900

    fix

--- src/utils/load.ts
+++ src/utils/load.ts
@@ -4,13 +4,13 @@ import path from 'path';
 import { BytecodeDump, MvrConfig } from './type';
 
 export const loadMvrConfig = async (): Promise<MvrConfig> => {
-  const configPath = path.resolve('mvr.config.json');
+  const configPath = path.resolve('../mvr.config.json');
   const configRaw = await fs.readFile(configPath, 'utf-8');
   return JSON.parse(configRaw) as MvrConfig;
 };
 
 export const loadBytecodeDump = async (): Promise<BytecodeDump> => {
-  const dumpPath = path.resolve('bytecode.dump.json');
+  const dumpPath = path.resolve('../bytecode.dump.json');
   const dumpRaw = await fs.readFile(dumpPath, 'utf-8');
   return JSON.parse(dumpRaw) as BytecodeDump;
 };
