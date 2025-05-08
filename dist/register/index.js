9b7a449c08897651b29bc70ea4e64a3d5ac1daa5
Lim Jet <57783762+daoauth@users.noreply.github.com>
2025-05-08 13:07:01 +0900

    build

--- dist/register/index.js
+++ dist/register/index.js
@@ -61997,81 +61997,232 @@ const core = __importStar(__nccwpck_require__(37484));
 const client_1 = __nccwpck_require__(70827);
 const transactions_1 = __nccwpck_require__(59417);
 const suins_1 = __nccwpck_require__(8154);
+const addAllMetadata_1 = __nccwpck_require__(57818);
 const getSigner_1 = __nccwpck_require__(73207);
 const load_1 = __nccwpck_require__(23469);
 const mvrResolver_1 = __nccwpck_require__(19076);
 const main = async () => {
     const config = await (0, load_1.loadMvrConfig)();
+    const provenance = await (0, load_1.loadProvenance)();
+    const deploy = await (0, load_1.loadDeploy)();
     const { signer, isGitSigner } = await (0, getSigner_1.getSigner)(config);
-    if (config.package_name && config.package_name.split('/').length !== 2) {
-        return;
+    if (config.app_name && config.app_name.split('/').length !== 2) {
+        process.exit(1);
     }
-    const suins = config.package_name?.split('/')[0].replace('@', '');
-    const pkgName = config.package_name?.split('/')[1];
+    if (config.network !== 'mainnet') {
+        core.setFailed(`❌ Network ${config.network} is not supported. Only mainnet is supported.`);
+        process.exit(1);
+    }
+    const suins = config.app_name?.split('/')[0].replace('@', '');
+    const pkgName = config.app_name?.split('/')[1];
     const client = new client_1.SuiClient({ url: (0, client_1.getFullnodeUrl)('mainnet') });
     const suinsClient = new suins_1.SuinsClient({
         client,
         network: 'mainnet',
     });
+    const { version } = await (0, load_1.loadUpgradeCap)(deploy.upgrade_cap, client);
     const record = await suinsClient.getNameRecord(suins.endsWith('.sui') ? suins : `${suins}.sui`);
     if (!record) {
         core.setFailed(`❌ Name record not found for ${suins}`);
-        return;
+        process.exit(1);
     }
-    const obj = await client.getObject({
+    const { data } = await client.getObject({
         id: record.nftId,
         options: { showOwner: true, showType: true },
     });
-    if (!obj) {
+    if (!data) {
         core.setFailed(`❌ Object not found for ${suins}`);
-        return;
+        process.exit(1);
     }
-    if ((typeof obj.data.owner === 'object' &&
-        'AddressOwner' in obj.data.owner &&
-        obj.data.owner.AddressOwner) !== config.owner) {
-        core.setFailed(`❌ Object ${record.nftId} is not owned by ${config.owner}. Current owner: ${obj.data.owner.AddressOwner}`);
-        return;
+    if ((typeof data.owner === 'object' &&
+        'AddressOwner' in data.owner &&
+        data.owner.AddressOwner) !== config.owner) {
+        core.setFailed(`❌ Object ${record.nftId} is not owned by ${config.owner}. Current owner: ${data.owner.AddressOwner}`);
+        process.exit(1);
     }
-    if (!obj.data.type?.endsWith('::SuinsRegistration')) {
+    if (!data.type?.endsWith('::SuinsRegistration')) {
         core.setFailed(`❌ Object ${record.nftId} is not a Suins object`);
-        return;
+        process.exit(1);
     }
     const registryObj = await suinsClient.getNameRecord('registry-obj@mvr');
-    const cache = await (0, mvrResolver_1.mvrResolver)(['@mvr/core', config.package_name], config.network);
+    const cache = await (0, mvrResolver_1.mvrResolver)(['@mvr/core', '@mvr/metadata', config.app_name], config.network);
     if (!registryObj || !registryObj.targetAddress) {
         core.setFailed(`❌ Registry object not found`);
-        return;
+        process.exit(1);
     }
-    if (cache[config.package_name]) {
-        // TODO Update
+    if (!cache['@mvr/core'] || !cache['@mvr/metadata']) {
+        core.setFailed(`❌ Package not found`);
+        process.exit(1);
     }
-    else {
+    if (!cache[config.app_name]) {
         const transaction = new transactions_1.Transaction();
         transaction.setSender(config.owner);
+        const registry = transaction.object(registryObj.targetAddress);
+        const nftId = transaction.object(record.nftId);
         const appCap = transaction.moveCall({
             target: `${cache['@mvr/core']}::move_registry::register`,
+            arguments: [registry, nftId, transaction.pure.string(pkgName), transaction.object.clock()],
+        });
+        transaction.add((0, addAllMetadata_1.addAllMetadata)(`${cache['@mvr/core']}::move_registry::set_metadata`, registry, appCap, config, deploy, provenance));
+        const packageInfo = transaction.moveCall({
+            target: `${cache['@mvr/metadata']}::package_info::new`,
+            arguments: [transaction.object(deploy.upgrade_cap)],
+        });
+        const git = transaction.moveCall({
+            target: `${cache['@mvr/metadata']}::git::new`,
             arguments: [
-                transaction.object(registryObj.targetAddress),
-                transaction.object(record.nftId),
-                transaction.pure.string(pkgName),
-                transaction.object.clock(),
+                transaction.pure.string(process.env.GIT_REPO ?? ''),
+                transaction.pure.string(process.env.GIT_SUBDIR ?? ''),
+                transaction.pure.string(process.env.GIT_COMMIT ?? ''),
             ],
         });
         transaction.moveCall({
-            target: `${cache['@mvr/core']}::move_registry::set_metadata`,
+            target: `${cache['@mvr/metadata']}::package_info::set_git_versioning`,
+            arguments: [packageInfo, transaction.pure.u64(version), git],
+        });
+        transaction.moveCall({
+            target: `${cache['@mvr/core']}::move_registry::assign_package`,
+            arguments: [registry, appCap, packageInfo],
+        });
+        const recipient = transaction.moveCall({
+            target: '0x2::tx_context::sender',
+        });
+        transaction.moveCall({
+            target: `${cache['@mvr/metadata']}::package_info::transfer`,
+            arguments: [packageInfo, recipient],
+        });
+        transaction.transferObjects([appCap], recipient);
+        const { input } = await client.dryRunTransactionBlock({
+            transactionBlock: await transaction.build({ client }),
+        });
+        transaction.setGasBudget(parseInt(input.gasData.budget));
+        const { digest: txDigest } = await client.signAndExecuteTransaction({
+            signer,
+            transaction,
+        });
+        const { effects: txEffect, objectChanges } = await client.waitForTransaction({
+            digest: txDigest,
+            options: { showEffects: true, showObjectChanges: true },
+        });
+        if (!txEffect || txEffect.status.status !== 'success') {
+            core.setFailed(`❌ Transaction failed: ${txDigest} - ${txEffect?.status.error ?? 'Unknown error'}`);
+            process.exit(1);
+        }
+        else {
+            const [appCap] = (objectChanges || []).filter(item => item.type === 'created' && item.objectType.endsWith('::app_record::AppCap'));
+            const [pkgInfo] = (objectChanges || []).filter(item => item.type === 'created' && item.objectType.endsWith('::package_info::PackageInfo'));
+            if (isGitSigner) {
+                const message = new TextEncoder().encode(JSON.stringify({ url: `https://suiscan.xyz/${config.network}/tx/${txDigest}` }));
+                await signer.signPersonalMessage(message, true);
+            }
+            core.info(`✅ App Cap: ${appCap.objectId}`);
+            core.info(`✅ Package Info: ${pkgInfo.objectId}`);
+            core.info(`✅ Transaction executed successfully: https://suiscan.xyz/${config.network}/tx/${txDigest}`);
+        }
+    }
+    else if (!!config.app_cap && !!config.pkg_info) {
+        const transaction = new transactions_1.Transaction();
+        transaction.setSender(config.owner);
+        const registry = transaction.object(registryObj.targetAddress);
+        const appCap = transaction.object(config.app_cap);
+        const packageInfo = transaction.object(config.pkg_info);
+        transaction.add((0, addAllMetadata_1.addAllMetadata)(`${cache['@mvr/core']}::move_registry::set_metadata`, registry, appCap, config, deploy, provenance));
+        transaction.moveCall({
+            target: `${cache['@mvr/metadata']}::package_info::unset_git_versioning`,
+            arguments: [packageInfo, transaction.pure.u64(parseInt(version) - 1)],
+        });
+        const git = transaction.moveCall({
+            target: `${cache['@mvr/metadata']}::git::new`,
             arguments: [
-                transaction.object(registryObj.targetAddress),
-                appCap,
-                transaction.pure.string('description'),
-                transaction.pure.string(config.package_desc),
+                transaction.pure.string(process.env.GIT_REPO ?? ''),
+                transaction.pure.string(process.env.GIT_SUBDIR ?? ''),
+                transaction.pure.string(process.env.GIT_COMMIT ?? ''),
             ],
         });
+        transaction.moveCall({
+            target: `${cache['@mvr/metadata']}::package_info::set_git_versioning`,
+            arguments: [packageInfo, transaction.pure.u64(version), git],
+        });
+        const { input } = await client.dryRunTransactionBlock({
+            transactionBlock: await transaction.build({ client }),
+        });
+        transaction.setGasBudget(parseInt(input.gasData.budget));
+        const { digest: txDigest } = await client.signAndExecuteTransaction({
+            signer,
+            transaction,
+        });
+        const { effects: txEffect } = await client.waitForTransaction({
+            digest: txDigest,
+            options: { showEffects: true },
+        });
+        if (!txEffect || txEffect.status.status !== 'success') {
+            core.setFailed(`❌ Transaction failed: ${txDigest} - ${txEffect?.status.error ?? 'Unknown error'}`);
+            process.exit(1);
+        }
+        else {
+            if (isGitSigner) {
+                const message = new TextEncoder().encode(JSON.stringify({ url: `https://suiscan.xyz/${config.network}/tx/${txDigest}` }));
+                await signer.signPersonalMessage(message, true);
+            }
+            core.info(`✅ Transaction executed successfully: https://suiscan.xyz/${config.network}/tx/${txDigest}`);
+        }
+    }
+    else {
+        core.info(`ℹ️ Detected existing MVR registration for ${config.app_name}`);
+        core.info(`ℹ️ Use app_cap + pkg_info to update metadata only`);
+        process.exit(1);
     }
 };
 main().catch(err => {
     core.setFailed(`❌ Error running deploy script: ${err}`);
     process.exit(1);
 });
+
+
+/***/ }),
+
+/***/ 57818:
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.addAllMetadata = exports.setMetaData = void 0;
+const setMetaData = (target, key, value, registryObj, appCap) => {
+    return (transaction) => {
+        return transaction.moveCall({
+            target,
+            arguments: [
+                registryObj,
+                appCap,
+                transaction.pure.string(key),
+                transaction.pure.string(value),
+            ],
+        });
+    };
+};
+exports.setMetaData = setMetaData;
+const addAllMetadata = (metadataTarget, registry, appCap, config, deploy, provenance) => {
+    const keys = [
+        ['description', config.app_desc],
+        ['homepage_url', config.homepage_url],
+        ['documentation_url', config.documentation_url],
+        ['icon_url', config.icon_url],
+        ['contact', config.contact],
+        ['deploy', JSON.stringify(deploy)],
+        ['provenance', JSON.stringify(provenance)],
+    ];
+    return (transaction) => {
+        let lastResult;
+        for (const [key, value] of keys) {
+            if (value) {
+                lastResult = transaction.add((0, exports.setMetaData)(metadataTarget, key, value, registry, appCap));
+            }
+        }
+        return lastResult;
+    };
+};
+exports.addAllMetadata = addAllMetadata;
 
 
 /***/ }),
@@ -62425,13 +62576,47 @@ exports.GitSigner = GitSigner;
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.loadBytecodeDump = exports.loadMvrConfig = void 0;
+exports.loadUpgradeCap = exports.loadProvenance = exports.loadDeploy = exports.loadBytecodeDump = exports.loadMvrConfig = void 0;
 const promises_1 = __importDefault(__nccwpck_require__(91943));
 const path_1 = __importDefault(__nccwpck_require__(16928));
+const core = __importStar(__nccwpck_require__(37484));
 const loadMvrConfig = async () => {
     const configPath = path_1.default.resolve('../mvr.config.json');
     const configRaw = await promises_1.default.readFile(configPath, 'utf-8');
@@ -62444,6 +62629,40 @@ const loadBytecodeDump = async () => {
     return JSON.parse(dumpRaw);
 };
 exports.loadBytecodeDump = loadBytecodeDump;
+const loadDeploy = async () => {
+    const dumpPath = path_1.default.resolve('../deploy.json');
+    const dumpRaw = await promises_1.default.readFile(dumpPath, 'utf-8');
+    return JSON.parse(dumpRaw);
+};
+exports.loadDeploy = loadDeploy;
+const loadProvenance = async () => {
+    const dumpPath = path_1.default.resolve('../mvr.intoto.jsonl');
+    const dumpRaw = await promises_1.default.readFile(dumpPath, 'utf-8');
+    return JSON.parse(dumpRaw);
+};
+exports.loadProvenance = loadProvenance;
+const loadUpgradeCap = async (id, client) => {
+    const { data } = await client.getObject({
+        id,
+        options: { showContent: true, showType: true },
+    });
+    if (!data ||
+        data.type !== '0x2::package::UpgradeCap' ||
+        data.content?.dataType !== 'moveObject') {
+        core.setFailed(`❌ Upgrade cap not found: ${id}`);
+        process.exit(1);
+    }
+    const fields = data.content.fields;
+    if (!fields.package) {
+        core.setFailed(`❌ 'package' field not found in UpgradeCap`);
+        process.exit(1);
+    }
+    return {
+        package: fields.package,
+        version: fields.version,
+    };
+};
+exports.loadUpgradeCap = loadUpgradeCap;
 
 
 /***/ }),
